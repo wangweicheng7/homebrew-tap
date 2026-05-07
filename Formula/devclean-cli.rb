a416737bcd9d633805e38757ea16fe4f10430b31
wangweicheng <809405366@qq.com>
2026-05-07 10:08:03 +0800

    chore: publish devclean-cli formula update

--- Formula/devclean-cli.rb
+++ Formula/devclean-cli.rb
@@ -1,15 +1,15 @@
 class DevcleanCli < Formula
   desc "macOS developer cleanup CLI (safe-first)"
   homepage "https://github.com/wangweicheng7/devclean-cli"
-  version "0.2.2"
+  version "0.2.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wangweicheng7/devclean-cli/releases/download/v0.2.2/devclean_0.2.2_darwin_arm64.tar.gz"
-      sha256 "76226dfae6e7cf15eb4513f6dd81a423cd215f0db887a857549914b3f964529c"
+      url "https://github.com/wangweicheng7/devclean-cli/releases/download/v0.2.3/devclean_0.2.3_darwin_arm64.tar.gz"
+      sha256 "4fd52b930b72f80f25cbe0efc970bcdadfeb5fd77439c5bcad6a40a6acea5c1a"
     else
-      url "https://github.com/wangweicheng7/devclean-cli/releases/download/v0.2.2/devclean_0.2.2_darwin_amd64.tar.gz"
-      sha256 "1883e3ecde07efca13f1fc9e07105628b65328d4cc6b3b861eebc466cbd95714"
+      url "https://github.com/wangweicheng7/devclean-cli/releases/download/v0.2.3/devclean_0.2.3_darwin_amd64.tar.gz"
+      sha256 "5f02bedb88370b196ba2024c39395aa6afd14d05ab3876076b869ae13364e321"
     end
   end
 
